6fa0fcd200cf7db804ca77bd56343a4afd760434
김현빈 <bin031001@gmail.com>
2025-08-03 05:25:52 +0900

    Update meteor.py and rename meteor images

--- objects/meteor.py
+++ objects/meteor.py
@@ -5,11 +5,11 @@ from objects.object import Object
 
 class Meteor(Object):
 
-    def __init__(self, image_path, image_size):
+    def __init__(self, image_path, image_size, speed):
         super().__init__(image_path, image_size)
 
         # 서브클래스들의 속도를 다르게 하기 위한 speed 속성 추가
-        self.speed = 0
+        self.speed = speed
 
         # x좌표를 무작위로 설정
         center_x = self.image.get_width() // 2
@@ -20,3 +20,27 @@ class Meteor(Object):
     # speed 값에 따라 각기 다른 속도로 하강
     def move(self, delta_seconds, speed):
         self.y += delta_seconds * speed
+
+
+class SmallMeteor(Meteor):
+
+    def __init__(
+        self, image_path="assets/images/meteor_small.png", image_size=10, speed=100
+    ):
+        super().__init__(image_path, image_size, speed)
+
+
+class MediumMeteor(Meteor):
+
+    def __init__(
+        self, image_path="assets/images/meteor_medium.png", image_size=30, speed=50
+    ):
+        super().__init__(image_path, image_size, speed)
+
+
+class BigMeteor(Meteor):
+
+    def __init__(
+        self, image_path="assets/images/meteor_big.png", image_size=10, speed=100
+    ):
+        super().__init__(image_path, image_size, speed)
